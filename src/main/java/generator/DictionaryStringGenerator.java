a779c58cd4b9776b7d0175d9574d34c090d36a83
Marco Monteleone <mar.monteleone@gmail.com>
2018-02-04 19:10:29 +0100

    added support for parametrized choice for the StringGenerator implementation

---
+++ src/main/java/generator/DictionaryStringGenerator.java
@@ -0,0 +1,27 @@
+package generator;
+
+public class DictionaryStringGenerator implements StringGenerator {
+
+	public DictionaryStringGenerator(String path){
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public String generateSourceName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateAttributeName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateAttributeValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
