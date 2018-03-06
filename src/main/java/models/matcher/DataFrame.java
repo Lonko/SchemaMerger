f6079c4a8cb47a88fa46a49e0096a92b86d3cac4
Marco Monteleone <mar.monteleone@gmail.com>
2018-03-06 02:11:14 +0100

    Refactoring

--- src/main/java/models/matcher/DataFrame.java
+++ src/main/java/models/matcher/DataFrame.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 import java.util.TreeSet;
 
 public class DataFrame {
-	
+
 	private List<String> attrCatalog = new ArrayList<>();
 	private List<String> attrSource = new ArrayList<>();
 	private List<Double> JSDs = new ArrayList<>();
@@ -22,10 +23,10 @@ public class DataFrame {
 	private List<Double> Match = new ArrayList<>();
 	private int nRows = 0;
 
-	public DataFrame(){
+	public DataFrame() {
 	}
-	
-	public DataFrame(DataFrame df){
+
+	public DataFrame(DataFrame df) {
 		Collections.copy(this.attrCatalog, df.getAttrCatalog());
 		Collections.copy(this.attrSource, df.getAttrSource());
 		Collections.copy(this.JSDs, df.getJSDs());
@@ -39,11 +40,11 @@ public class DataFrame {
 		Collections.copy(this.MIc, df.getMIc());
 		Collections.copy(this.Match, df.getMatch());
 	}
-	
-	public void addRow(Features features, String aCatalog, String aSource){
+
+	public void addRow(Features features, String aCatalog, String aSource) {
 		this.attrCatalog.add(aCatalog);
 		this.attrSource.add(aSource);
-		
+
 		this.JSDs.add(features.getSourceJSD());
 		this.JSDw.add(features.getWebsiteJSD());
 		this.JSDc.add(features.getCategoryJSD());
@@ -55,57 +56,61 @@ public class DataFrame {
 		this.MIs.add(features.getSourceMI());
 		this.MIw.add(features.getWebsiteMI());
 		this.MIc.add(features.getCategoryMI());
-		
+
 		this.Match.add(features.getMatch());
-		
+
 		this.nRows += 1;
 	}
-	
-	public String getStringAtIndex(String field, int index){
+
+	public String getStringAtIndex(String field, int index) {
 		String value = null;
-		if(field.equals("attrCatalog"))
+		if (field.equals("attrCatalog"))
 			value = this.attrCatalog.get(index);
-		if(field.equals("attrSource"))
+		if (field.equals("attrSource"))
 			value = this.attrSource.get(index);
 		return value;
 	}
-	
-	public void removeByIndexes(List<Integer> indexes){
-		for(int index : indexes)
+
+	public void removeByIndexes(List<Integer> indexes) {
+		for (int index : indexes)
 			removeByIndex(index);
 	}
-	
-	public List<Integer> getSourceRanges(){
+
+	// each "range" is the range of indexes of rows that refer to matches for
+	// the same source attribute
+	public List<Integer> getSourceRanges() {
 		List<Integer> ranges = new ArrayList<>();
-		String attr = ""; 
-		
-		for(int i = 0; i < this.attrSource.size(); i++){
-			if(!this.attrSource.get(i).equals(attr)){
+		String attr = "";
+
+		for (int i = 0; i < this.attrSource.size(); i++) {
+			if (!this.attrSource.get(i).equals(attr)) {
 				ranges.add(i);
 				attr = this.attrSource.get(i);
 			}
 		}
 		ranges.add(this.attrSource.size());
-		
+
 		return ranges;
 	}
-	
-	public Set<Integer> getIndexesByValue(String field, String value){
+
+	public Set<Integer> getIndexesByValue(String field, String value) {
 		Set<Integer> indexes = new TreeSet<>();
-		
-		if(field.equals("attrCatalog")){
-			for(int i = 0; i < this.attrCatalog.size(); i++)
-				if(this.attrCatalog.get(i).equals(value)) indexes.add(i);
+
+		if (field.equals("attrCatalog")) {
+			for (int i = 0; i < this.attrCatalog.size(); i++)
+				if (this.attrCatalog.get(i).equals(value))
+					indexes.add(i);
 		}
-		if(field.equals("attrSource")){
-			for(int i = 0; i < this.attrSource.size(); i++)
-				if(this.attrSource.get(i).equals(value)) indexes.add(i);
+		if (field.equals("attrSource")) {
+			for (int i = 0; i < this.attrSource.size(); i++)
+				if (this.attrSource.get(i).equals(value))
+					indexes.add(i);
 		}
-		
+
 		return indexes;
 	}
-	
-	public void removeByIndex(int index){
+
+	public void removeByIndex(int index) {
 		this.attrCatalog.remove(index);
 		this.attrSource.remove(index);
 		this.JSDs.remove(index);
@@ -120,12 +125,24 @@ public class DataFrame {
 		this.Match.remove(index);
 		this.nRows -= 1;
 	}
-	
-	public void updateMatchProbabilities(double[] probs){
+
+	public void updateMatchProbabilities(double[] probs) {
 		List<Double> newProbs = new ArrayList<>();
-		for(double p : probs)
+		for (double p : probs)
 			newProbs.add(p);
 		this.Match = newProbs;
+	}
+
+	// remove rows where there are null values
+	public void removeNull() {
+	    List<Integer> indexes = new ArrayList<>();
+		for (int i = 0; i < this.nRows; i++)
+			if (Stream.of(this.JSDs, this.JSDw, this.JSDc, this.JCs, 
+			              this.JCw, this.JCc, this.MIs, this.MIw, this.MIc)
+			          .allMatch(x -> x == null))
+				indexes.add(i);
+		
+		removeByIndexes(indexes);
 	}
 
 	public List<String> getAttrCatalog() {
@@ -175,32 +192,23 @@ public class DataFrame {
 	public List<Double> getMatch() {
 		return Match;
 	}
-	
-	public List<String> toCSVFormat(){
+
+	public List<String> toCSVFormat() {
 		List<String> rows = new ArrayList<>();
-		for(int i = 0; i < this.nRows; i++){
-			rows.add(this.attrSource.get(i).replace(",", "#;#")
-						+","+this.attrCatalog.get(i).replace(",", "#;#")
-						+","+this.JSDs.get(i)
-						+","+this.JSDw.get(i)
-						+","+this.JSDc.get(i)
-						+","+this.JCs.get(i)
-						+","+this.JCw.get(i)
-						+","+this.JCc.get(i)
-						+","+this.MIs.get(i)
-						+","+this.MIw.get(i)
-						+","+this.MIc.get(i)
-						+","+this.Match.get(i));
+		for (int i = 0; i < this.nRows; i++) {
+			rows.add(this.attrSource.get(i).replace(",", "#;#") + "," + this.attrCatalog.get(i).replace(",", "#;#") + ","
+					+ this.JSDs.get(i) + "," + this.JSDw.get(i) + "," + this.JSDc.get(i) + "," + this.JCs.get(i) + "," + this.JCw.get(i)
+					+ "," + this.JCc.get(i) + "," + this.MIs.get(i) + "," + this.MIw.get(i) + "," + this.MIc.get(i) + ","
+					+ this.Match.get(i));
 		}
 		return rows;
 	}
-	
-	public List<String> toCSVFormatSlim(){
+
+	public List<String> toCSVFormatSlim() {
 		List<String> rows = new ArrayList<>();
-		for(int i = 0; i < this.nRows; i++){
-			rows.add(this.attrSource.get(i).replace(",", "#;#")
-						+","+this.attrCatalog.get(i).replace(",", "#;#")
-						+","+this.Match.get(i));
+		for (int i = 0; i < this.nRows; i++) {
+			rows.add(this.attrSource.get(i).replace(",", "#;#") + "," + this.attrCatalog.get(i).replace(",", "#;#") + ","
+					+ this.Match.get(i));
 		}
 		return rows;
 	}
